3ea438707e10cd5ce195fb2c232f90d5ddff3132
Haris Adiyatma Farhan <111201911669@mhs.dinus.ac.id>
2022-01-18 11:48:03 +0700

    uas

---
+++ UTSQL/app/src/main/java/com/example/utsql/Util/ServerAPI.java
@@ -0,0 +1,7 @@
+package com.example.utsql.Util;
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.100.35/crud_uas/view_data.php";
+    public static final String URL_INSERT = "http://10.0.2.2/crud_uas/create_data.php";
+    public static final String URL_DELETE = "http://10.0.2.2/crud_uas/delete_data.php";
+    public static final String URL_UPDATE = "http://10.0.2.2/crud_uas/update_data.php";
+}
